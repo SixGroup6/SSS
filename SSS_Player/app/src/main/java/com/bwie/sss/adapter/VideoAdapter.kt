8c742afa0bafce1ad4e5d3b6fa33dee288df29aa
reforgetYZ <31436453+reforgetYZ@users.noreply.github.com>
2017-11-23 15:01:04 +0800

    yz

--- SSS_Player/app/src/main/java/com/bwie/sss/adapter/VideoAdapter.kt
+++ SSS_Player/app/src/main/java/com/bwie/sss/adapter/VideoAdapter.kt
@@ -1,12 +1,29 @@
 package com.bwie.sss.adapter
 
 import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
 import com.bwie.sss.bean.VideoBean
 
 /**
  * Created by 燕子 on 2017/11/23.
  */
-class VideoAdapter(context:Context,list:VideoBean.Issue) {
+class VideoAdapter(context:Context,list:VideoBean.Issue): RecyclerView.Adapter<VideoAdapter.ViewHoler>() {
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHoler {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHoler?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHoler: RecyclerView.ViewHolder(item:View) {
+
+    }
 
 
 }
