1543bf5921ff6024d6c736cc2466e404d8ac6fba
bihongxin <1402242966@qq.com>
2017-11-30 15:28:08 +0800

    bhx1130

--- SSS_Player/app/src/main/java/com/bwie/sss/activity/DetailActivity.kt
+++ SSS_Player/app/src/main/java/com/bwie/sss/activity/DetailActivity.kt
@@ -3,14 +3,20 @@ package com.bwie.sss.activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import cn.jzvd.JZVideoPlayerStandard
 
 import com.bwie.sss.R
 import com.bwie.sss.bean.VideoBean
+import com.bwie.sss.util.ObjectSaveUtils
+import com.bwie.sss.util.SUtils
 import com.bwie.sss.util.SpUtils
+import com.jaeger.library.StatusBarUtil
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_detail.*
+import zlc.season.rxdownload2.RxDownload
 
 class DetailActivity : AppCompatActivity() {
 
@@ -19,29 +25,65 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        bean = intent.getParcelableExtra<VideoBean>("data")
+        StatusBarUtil.setTranslucent(this,0)
         initData()
+        setListener()
 
-        /*var preferences = SpUtils(this).prefs
-        var islogin = preferences.getBoolean("islogin", false)
+    }
+    //初始化数据
+    fun initData(){
+        bean = intent.getParcelableExtra<VideoBean>("data")
+        jc_video.setUp(bean?.playUrl, JZVideoPlayerStandard.SCREEN_LAYOUT_NORMAL,"")
+        jc_video.thumbImageView.scaleType= ImageView.ScaleType.FIT_XY
+        Picasso.with(this).load(bean?.feed).into(jc_video.thumbImageView)
+        tv_title.setText(bean?.title)
+        tv_describe.setText(bean?.description)
+        iv_background.scaleType=ImageView.ScaleType.FIT_XY
+        Picasso.with(this).load(bean?.blurred).into(iv_background)
+    }
+    //点击事件
+    fun setListener(){
+        var preferences = SpUtils(this).prefs
+        ll_down.setOnClickListener(View.OnClickListener {
+            var islogin = preferences.getBoolean("islogin", false)
         if (islogin) {
-            //详情页
-            startActivity(Intent(this,DetailActivity::class.java))
-
+           //下载
+            downVideo()
         } else {
             //登录
             var intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-        }*/
+        }
+        })
+        iv_return.setOnClickListener{
+            finish()
+        }
     }
-    //初始化数据
-    fun initData(){
-        jc_video.setUp(bean.playUrl, JZVideoPlayerStandard.SCREEN_LAYOUT_NORMAL,"")
-        jc_video.thumbImageView.scaleType= ImageView.ScaleType.FIT_XY
-        Picasso.with(this).load(bean.feed).into(jc_video.thumbImageView)
-        tv_title.setText(bean.title)
-        tv_describe.setText(bean.description)
-        iv_background.scaleType=ImageView.ScaleType.FIT_XY
-        Picasso.with(this).load(bean.blurred).into(iv_background)
+    //下载
+    fun downVideo(){
+        //点击下载
+        var url = bean.playUrl?.let { it1 -> SUtils.getInstance(this, "downloads").getString(it1) }
+        if (url.equals("")) {
+            var count = SUtils.getInstance(this, "downloads").getInt("count")
+            if (count != -1) {
+                count = count.inc()
+            } else {
+                count = 1
+            }
+            SUtils.getInstance(this, "downloads").put("count", count)
+            ObjectSaveUtils.saveObject(this, "download$count", bean)
+            addMission(bean.playUrl,count)
+        } else {
+            Toast.makeText(this@DetailActivity,"该视频已经缓存过了",0).show()
+        }
+    }
+    private fun addMission(playUrl: String?, count: Int) {
+        RxDownload.getInstance(this).serviceDownload(playUrl,"download$count").subscribe({
+            Toast.makeText(this@DetailActivity,"开始下载",0).show()
+            SUtils.getInstance(this, "downloads").put(bean.playUrl.toString(),bean.playUrl.toString())
+            SUtils.getInstance(this, "download_state").put(playUrl.toString(), true)
+        }, {
+            Toast.makeText(this@DetailActivity,"添加任务失败",0).show()
+        })
     }
 }
--- SSS_Player/app/src/main/java/com/bwie/sss/fragment/Fragment_wd.kt
+++ SSS_Player/app/src/main/java/com/bwie/sss/fragment/Fragment_wd.kt
@@ -1,8 +1,12 @@
 package com.bwie.sss.fragment
 
+import android.content.Intent
+import android.view.View
 import com.bwie.sss.R
+import com.bwie.sss.activity.CacheActivity
 import com.bwie.sss.presenter.P_UpData
 import com.bwie.sss.view.IView_Main
+import kotlinx.android.synthetic.main.fragment_wd.*
 
 /**
  * 1:类的用途
@@ -11,6 +15,9 @@ import com.bwie.sss.view.IView_Main
  */
 class Fragment_wd : BaseFragment<IView_Main,P_UpData<IView_Main>>(){
     override fun initData() {
+        tv_save.setOnClickListener{
+            startActivity(Intent(activity,CacheActivity::class.java))
+        }
     }
 
     override fun getPresenter(): P_UpData<IView_Main>? {
